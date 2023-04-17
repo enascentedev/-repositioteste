cd5aa3b076069aa46f03606393489dc69665ac25
emanuelnascente <emanuelalvorada@gmail.com>
2023-04-17 00:19:09 -0300

    estudos da noite

---
+++ exercicio10.php
@@ -0,0 +1,15 @@
+Exercício 10
+● Crie um arquivo PHP;
+● Crie uma operação que utiliza subtração ( - ), divisão ( / ) e multiplicação
+● Armazene todos os valores em variáveis;
+● Imprima o resultado final na tela;
+<?php
+
+// -, /, *
+$a = 10;
+$x = 5;
+$z = 9;
+
+$operacao = ($x - $z) / $a * $z;
+
+echo $operacao;
---
+++ exercicio13.php
@@ -0,0 +1,10 @@
+<?php
+
+  $a = 14;
+  $b = 2;
+  $c = 3;
+
+  echo $a % $b;
+  echo "\n";
+  echo $a % $c;
+  echo "\n";
