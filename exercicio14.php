886e3b7305d41da5338e69d99b647d390015e722
emanuelnascente <emanuelalvorada@gmail.com>
2023-04-18 07:27:25 -0300

    adicionado exercicios

---
+++ exercicio14.php
@@ -0,0 +1,11 @@
+Exercício 14
+● Crie um arquivo PHP;
+● Crie uma variável saudação, nome e outra de sobrenome;
+● Imprima com echo a concatenação de saudação, nome e sobrenome;
+<?php
+$boasvindas= "seja bem vindo";
+$nome= "Emanuel";
+
+echo $boasvindas . "\n" .$nome;
+
+
---
+++ exercicio15.php
@@ -0,0 +1,13 @@
+Exercício 15
+● Crie uma operação que retorne falso com igualdade;
+● Crie uma operação que retorne verdadeiro com igualdade;
+<?php
+$a= 4;
+$b= 5;
+$c= 4;
+if($a==$b) {
+  echo "a é igual a b";
+}
+if ($a==$c) {
+  echo "a é igual a c";
+}
