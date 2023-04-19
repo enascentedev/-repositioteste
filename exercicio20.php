15b9e87b0a5be6b72b457d4997c7447ca6f69358
emanuelnascente <emanuelalvorada@gmail.com>
2023-04-18 22:58:46 -0300

    noite 18.04.23

---
+++ exercicio20.php
@@ -0,0 +1,12 @@
+Exercício 20
+● Atribua dois números a variáveis distintas;
+● Faça uma comparação de menor ou igual com o operador ternário;
+● Imprima resultados para ambas as possibilidades;
+<?php
+$a=10;
+$b=5;
+if ($a>$b){
+  echo " a é maior que b" . "\n";
+} else {
+  echo "b é maior que a " . "\n";
+}
